2bc3988965cbf472ef795c7682a8a010e77dac54
TamirAvraham <tamir.avrah@gmail.com>
2022-11-20 10:34:54 +0200

    added second part of the task to header file and removed pram names in it

--- ConsoleApplication2/Vector.h
+++ ConsoleApplication2/Vector.h
@@ -6,11 +6,16 @@ private:
 	int _size;
 	int _capacity;
 	int _resizeFactor;
+	void resize();//this is to change the length of a vector
+	void resize(int n);
 public:
-	Vector(int n);
+	Vector(int);
 	~Vector();
 	int size()const;
 	int capacity()const;
 	bool empty()const;
+	void push_back(const int&);
+	int pop_back();
+	void reserve(int);
 };
 
